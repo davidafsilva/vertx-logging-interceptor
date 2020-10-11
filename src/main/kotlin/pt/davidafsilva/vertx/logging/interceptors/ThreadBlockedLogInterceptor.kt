251cfbd62194f4cdea377679a43bc073a91f56f6
David Silva <david@davidafsilva.pt>
2020-10-11 11:40:31 +0100

    adjusts thread names at thread metric label

--- src/main/kotlin/pt/davidafsilva/vertx/logging/interceptors/ThreadBlockedLogInterceptor.kt
+++ src/main/kotlin/pt/davidafsilva/vertx/logging/interceptors/ThreadBlockedLogInterceptor.kt
@@ -1,7 +1,7 @@
 package pt.davidafsilva.vertx.logging.interceptors
 
+import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.Tag
 import io.vertx.micrometer.backends.BackendRegistries
 import pt.davidafsilva.vertx.logging.LogPropagation
 import pt.davidafsilva.vertx.logging.NoOpLogInterceptor
@@ -12,14 +12,19 @@ class ThreadBlockedLogInterceptor @JvmOverloads constructor(
 
     companion object {
         private val LOGGER_NAME = io.vertx.core.impl.BlockedThreadChecker::class.java.canonicalName
-        private const val TEMPLATE = "Thread T has been blocked for M ms, time limit is L ms"
+        private const val TEMPLATE = "Thread E has been blocked for M ms, time limit is L ms"
         private const val MIN_LENGTH = TEMPLATE.length
         private const val MESSAGE_PREFIX_INDEX = 0
         private const val MESSAGE_PREFIX = "Thread"
+        private const val MESSAGE_BLOCKED_THREAD_ENTRY_INDEX = 1
+        private const val MESSAGE_BLOCKED_THREAD_ENTRY_THREAD_PREFIX = "Thread["
+        private const val MESSAGE_BLOCKED_THREAD_ENTRY_THREAD_SUFFIX = "]"
+        private const val MESSAGE_BLOCKED_THREAD_ENTRY_DELIMITER = "="
         private const val MESSAGE_BLOCKED_SUB_STR_INDEX = 2
         private const val MESSAGE_BLOCKED_SUB_STR = "has been blocked for"
 
         private const val VERTX_THREAD_BLOCKED_COUNTER_NAME = "vertx_thread_blocked"
+        private const val VERTX_THREAD_BLOCKED_COUNTER_DESC = "Number of thread blocks detected"
         private const val THREAD_TAG = "thread"
     }
 
@@ -30,10 +35,12 @@ class ThreadBlockedLogInterceptor @JvmOverloads constructor(
         return LogPropagation.CONTINUE // don't block the logging
     }
 
-    private fun incrementThreadBlockedCounter(blockedThread: String) = registry.counter(
-        VERTX_THREAD_BLOCKED_COUNTER_NAME,
-        listOf(Tag.of(THREAD_TAG, blockedThread))
-    ).increment()
+    private fun incrementThreadBlockedCounter(blockedThread: String) = Counter
+        .builder(VERTX_THREAD_BLOCKED_COUNTER_NAME)
+        .description(VERTX_THREAD_BLOCKED_COUNTER_DESC)
+        .tag(THREAD_TAG, blockedThread)
+        .register(registry)
+        .increment()
 
     private fun getBlockedThread(message: String): String? {
         if (message.length < MIN_LENGTH) return null
@@ -42,6 +49,28 @@ class ThreadBlockedLogInterceptor @JvmOverloads constructor(
         val valid = parts.size == 3 &&
             parts[MESSAGE_PREFIX_INDEX] == MESSAGE_PREFIX &&
             parts[MESSAGE_BLOCKED_SUB_STR_INDEX].startsWith(MESSAGE_BLOCKED_SUB_STR)
-        return if (valid) parts[1] else null
+
+        return if (valid) getThreadNameFromEntry(parts[MESSAGE_BLOCKED_THREAD_ENTRY_INDEX]) else null
+    }
+
+    private fun getThreadNameFromEntry(entry: String): String {
+        // entry is formatted through AbstractMap.SimpleEntry toString
+        return when {
+            // default format: Thread[<thread_info_here>]=<task>
+            entry.startsWith(MESSAGE_BLOCKED_THREAD_ENTRY_THREAD_PREFIX) ->
+                entry.substring(
+                    // remove Thread[
+                    MESSAGE_BLOCKED_THREAD_ENTRY_THREAD_PREFIX.length,
+                    // up to ]=
+                    entry.indexOf("$MESSAGE_BLOCKED_THREAD_ENTRY_THREAD_SUFFIX$MESSAGE_BLOCKED_THREAD_ENTRY_DELIMITER")
+                )
+            // probably a custom thread formatting was defined
+            else -> {
+                // might need adjusting for threads with '=' chars within its toString()
+                val entryDelimiter = entry.indexOf(MESSAGE_BLOCKED_THREAD_ENTRY_DELIMITER)
+                entry.substring(0, entryDelimiter)
+            }
+        }
+
     }
 }
