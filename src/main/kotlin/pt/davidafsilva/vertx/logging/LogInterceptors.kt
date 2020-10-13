02aec2680840f887dc57b49a360812e24b47931b
David Silva <david@davidafsilva.pt>
2020-10-13 18:15:14 +0100

    Fixes interceptors vararg register variant ignoring the log level

--- src/main/kotlin/pt/davidafsilva/vertx/logging/LogInterceptors.kt
+++ src/main/kotlin/pt/davidafsilva/vertx/logging/LogInterceptors.kt
@@ -11,13 +11,17 @@ object LogInterceptors {
         allLogLevels.forEach { register(it, logInterceptor) }
     }
 
+    fun register(vararg logInterceptors: LogInterceptor) {
+        logInterceptors.forEach(::register)
+    }
+
     fun register(level: Level, logInterceptor: LogInterceptor) {
         interceptors.computeIfAbsent(level) { mutableListOf() }
             .add(logInterceptor)
     }
 
     fun register(level: Level, vararg logInterceptors: LogInterceptor) {
-        logInterceptors.forEach(::register)
+        logInterceptors.forEach { register(level, it) }
     }
 
     fun interceptorsFor(level: Level): List<LogInterceptor> = interceptors[level] ?: noInterceptors
