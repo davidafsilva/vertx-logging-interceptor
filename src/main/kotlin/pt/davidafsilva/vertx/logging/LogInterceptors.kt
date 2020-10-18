0a41a10657329f4d9bbaaacfe5a045b9dfc8e935
David Silva <david@davidafsilva.pt>
2020-10-18 11:39:18 +0100

    Improves Java interface for LogInterceptors

--- src/main/kotlin/pt/davidafsilva/vertx/logging/LogInterceptors.kt
+++ src/main/kotlin/pt/davidafsilva/vertx/logging/LogInterceptors.kt
@@ -5,22 +5,26 @@ object LogInterceptors {
     private val allLogLevels = LogLevel.values()
     private val interceptors = mutableMapOf<LogLevel, MutableList<LogInterceptor>>()
 
+    @JvmStatic
     fun register(logInterceptor: LogInterceptor) {
         allLogLevels.forEach { register(it, logInterceptor) }
     }
 
+    @JvmStatic
     fun register(vararg logInterceptors: LogInterceptor) {
         logInterceptors.forEach(::register)
     }
 
+    @JvmStatic
     fun register(level: LogLevel, logInterceptor: LogInterceptor) {
         interceptors.computeIfAbsent(level) { mutableListOf() }
             .add(logInterceptor)
     }
 
+    @JvmStatic
     fun register(level: LogLevel, vararg logInterceptors: LogInterceptor) {
         logInterceptors.forEach { register(level, it) }
     }
 
-    fun interceptorsFor(level: LogLevel): List<LogInterceptor> = interceptors[level] ?: noInterceptors
+    internal fun interceptorsFor(level: LogLevel): List<LogInterceptor> = interceptors[level] ?: noInterceptors
 }
