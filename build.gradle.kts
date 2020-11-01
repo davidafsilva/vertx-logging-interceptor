4cff013a9114bf0fff48fe32a7689583f49b9177
David Silva <david@davidafsilva.pt>
2020-11-01 13:07:35 +0000

    temporarily disables coveralls integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     kotlin("jvm")
     jacoco
     `maven-publish`
-    id("com.github.nbaztec.coveralls-jacoco")
+    //id("com.github.nbaztec.coveralls-jacoco")
     id("pl.allegro.tech.build.axion-release")
     id("com.jfrog.bintray")
 }
